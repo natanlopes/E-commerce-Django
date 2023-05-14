8fe8a1176630db5662da3e9f8fa5dbf3b7a18714
Natan Lopes <natan1918@gmail.com>
2023-05-14 11:52:19 -0300

    Adicionar rotas para administração e debug_toolbar

--- loja/urls.py
+++ loja/urls.py
@@ -23,7 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
-
+#Adicionar rotas para administração e debug_toolbar
 
 if settings.DEBUG:
     import debug_toolbar
