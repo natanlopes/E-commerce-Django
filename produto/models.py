34ab88fca8e02a01ce2f6227715b11a602e6a02a
Natan Lopes <natan1918@gmail.com>
2023-05-14 20:19:12 -0300

    cirando a classe produto

--- produto/models.py
+++ produto/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Produto(models.Model):
+    nome = models.CharField(max_length=225)
+    descricao_curta = models.TextField(max_length=225)
+    descricao_longa = models.TextField()
+    imagem = models.ImageField(upload_to='produto_imagens/%Y/%m',blank=True,null=True)
+    preco_marting = models.FloatField()
+    preco_marting_promocional = models.FloatField(default=0)
+    slug = models.SlugField(unique=True)
+    tipo = models.CharField(default='V',max_length=1,choices=(('V','Variação'),('S','Simples'),))
