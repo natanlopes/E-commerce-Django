4be83d637dd32e2ef2a50b50aa0a6850f01070f3
Natan Lopes <natan1918@gmail.com>
2023-06-29 20:25:55 -0300

    detalhe.html colocando os preços

--- produto/views.py
+++ produto/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from django.views import View
 from . import models
 class ListaProdutos(ListView):
@@ -8,9 +9,11 @@ class ListaProdutos(ListView):
    template_name = 'produto/lista.html'
    context_object_name = 'produtos'
    paginate_by = 7
-class DetalheProduto(View):
-    def get(self, *args, **kwargs):
-        return HttpResponse('DetalheProduto')
+class DetalheProduto(DetailView):
+    model = models.Produto
+    template_name = 'produto/detalhe.html'
+    context_object_name = 'produto'
+    slug_url_kwarg = "slug"
 class AdcionarAoCarinho(View):
     def get(self, *args, **kwargs):
         return HttpResponse('AdcionarAoCarinho')
